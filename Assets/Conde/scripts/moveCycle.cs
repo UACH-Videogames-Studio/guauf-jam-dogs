4289824e06485fb3240064f7716f215c160c89a7
Adora <im.stariluz@gmail.com>
2024-10-13 19:47:56 -0600

    Control direction from line

--- Assets/Conde/scripts/moveCycle.cs
+++ Assets/Conde/scripts/moveCycle.cs
@@ -6,16 +6,19 @@ public class MoveCycle : MonoBehaviour
 {
     public Vector2 direction = Vector2.right; // Dirección de movimiento
     public float speed = 1f; // Velocidad del auto
-    public float size = 1f; // Tamaño del auto
+    public float carWidth = 1f; // Tamaño del auto
 
     private Vector3 leftEdge; // Límite izquierdo
     private Vector3 rightEdge; // Límite derecho
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         // Obtiene los límites de la pantalla
-        leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero)-2*Vector3.right;
+        leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
         rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        spriteRenderer=GetComponent<SpriteRenderer>();
+        carWidth = spriteRenderer.bounds.size.x;
     }
 
     private void Update()
@@ -24,22 +27,31 @@ public class MoveCycle : MonoBehaviour
         transform.Translate(direction * speed * Time.deltaTime);
 
         // Verifica si el auto está completamente fuera de los límites derecho o izquierdo
-        if (direction.x > 0 && (transform.position.x - size) > rightEdge.x)
+        if (direction.x > 0 && transform.position.x > rightEdge.x)
         {
             // Reposiciona el auto al límite izquierdo
             Vector3 position = transform.position;
-            position.x = leftEdge.x - size; // Respawn al lado izquierdo
+            position.x = leftEdge.x - carWidth; // Respawn al lado izquierdo
             transform.position = position;
         }
-        else if (direction.x < 0 && (transform.position.x + size) < leftEdge.x)
+        else if (direction.x < 0 && (transform.position.x + carWidth) < leftEdge.x)
         {
             // Reposiciona el auto al límite derecho
             Vector3 position = transform.position;
-            position.x = rightEdge.x + size; // Respawn al lado derecho
+            position.x = rightEdge.x; // Respawn al lado derecho
             transform.position = position;
         }
     }
 
+    public void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection; // Establece la velocidad
+        if(direction.x>=0){
+            spriteRenderer.flipX=false;
+        }else{
+            spriteRenderer.flipX=true;
+        }
+    }
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed; // Establece la velocidad

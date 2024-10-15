f8086c948d30b6b1ad56f625b331387f4c75840f
Adora <im.stariluz@gmail.com>
2024-10-14 23:00:54 -0600

    Fix timer death and hardness of level 3

--- Assets/Dogger/Cinematics/GameOver/UIAnimationOutScreen.cs
+++ Assets/Dogger/Cinematics/GameOver/UIAnimationOutScreen.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
 public class CreditsOpenAnimation : MonoBehaviour
 {
